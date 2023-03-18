a66d3d7cac2585a9642275ab6f2f73ab42cb0874
Tim-Snugget <41423449+Tim-Snugget@users.noreply.github.com>
2023-03-18 17:38:17 +0100

    Explosion and particles when weapon explodes

--- Assets/Scripts/Weapons/BazookaProjectile.cs
+++ Assets/Scripts/Weapons/BazookaProjectile.cs
@@ -4,35 +4,8 @@ using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 
-public class BazookaProjectile : NetworkBehaviour
+public class BazookaProjectile : AWeapon
 {
-    public Rigidbody rb;
-
-    #region Server
-
-    /// <summary>
-    /// Tells all the client to apply said angle and power to the projectile
-    /// </summary>
-    /// <param name="angle">Angle</param>
-    /// <param name="power">Normalized power</param>
-    [ClientRpc]
-    public void RpcFireBazooka(float angle, float power)
-    {
-        Vector3 vec = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle), 0);
-        float p = Mathf.Lerp(20f, 1000f, power);
-        rb.AddForce(vec * p);
-    }
-    
-    
-    /// <summary>
-    /// Destroy the object
-    /// </summary>
-    [Server]
-    private void DestroySelf()
-    {
-        NetworkServer.Destroy(gameObject);
-    }
-
 
     /// <summary>
     /// When the projectile collides with another game object
@@ -41,9 +14,8 @@ public class BazookaProjectile : NetworkBehaviour
     [ServerCallback]
     private void OnTriggerEnter(Collider other)
     {
-        // TODO explosion
         DestroySelf();
     }
-    
-    #endregion
+
+
 }
